cc56c46364da70fcf63866a318e35388e7fd0d08
c2vasu <c2vasu@yahoo.co.in>
2015-03-08 11:56:08 -0700

    Integrated Spring with Hibernate and Spring Data JPA

---
+++ src/main/java/ca/store/product/controller/IndexController.java
@@ -0,0 +1,13 @@
+package ca.store.product.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+	
+	@RequestMapping("/index")
+	public String index() {
+		return "/WEB-INF/jsp/index.jsp";
+	}
+}
--- src/main/webapp/WEB-INF/jsp/index.jsp
+++ src/main/webapp/WEB-INF/jsp/index.jsp
@@ -7,6 +7,6 @@
 <title>Insert title here</title>
 </head>
 <body>
-It Works !!!
+  Hello World!!
 </body>
 </html>
